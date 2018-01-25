4c78535ba9eaea05f2402691941eebbfc4e43983
rahuljain5 <rjain.rahul5@gmail.com>
2018-01-26 01:15:29 +0530

    Added model call and removed everything else

--- app.js
+++ app.js
@@ -2,19 +2,14 @@ var http = require('http');
 var axi = require("axios");
 var qs = require('qs');
 var S = require('string');
+var padder = require('zpad');
+var fetchObj = require('./models/Result_Fetch.js')
 var vtu = require('./utils/helper.js')
-axi.post('http://results.vtu.ac.in/results17/result_page.php', qs.stringify({ usn: 'usn_here' }))
-  .then(function (response) {
-    var str = S(response.data);
-    if(str.contains("alert(\"University Seat Number is not available or Invalid..!\");")==false && str.contains("alert(\"Invalid USN Format..\");")==false)
-  	{
-  		vtu.parse_html(str);
-		}
- 	  else
-     {
-      console.log("Wrong USN macha!");
-      }
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+try {
+  // Call the Async process that will fetch all class results 
+  fetchObj.scrape(7 digit USN here);  
+}
+catch (error) { throw(error);
+exit();
+}
+console.log('Task Finished');
