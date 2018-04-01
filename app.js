f2cf79c8c2653883d018d8b9ae484cb0f300c680
rahuljain5 <rjain.rahul5@gmail.com>
2018-04-01 20:46:00 +0530

    USN Update

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ var axi = require("axios");
 var qs = require('qs');
 var S = require('string');
 var padder = require('zpad');
-var fetchObj = require('./models/Result_Fetch.js')
-var vtu = require('./utils/helper.js')
+var fetchObj = require('./utils/Result_Fetch.js')
 try {
-  // Call the Async process that will fetch all class results 
-  fetchObj.scrape(7 digit USN here);  
+  // Call the Async process that will fetch all class results
+  fetchObj.scrape("1ox15cs");  
 }
 catch (error) { throw(error);
 exit();
