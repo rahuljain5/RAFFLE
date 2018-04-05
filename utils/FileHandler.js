a387a2e7fabc031e7bc87ae6f2418f542e779398
rahuljain5 <rjain.rahul5@gmail.com>
2018-04-05 19:25:55 +0530

    File Move Path Change

--- utils/FileHandler.js
+++ utils/FileHandler.js
@@ -10,7 +10,7 @@ const FileUploader = () =>
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
       var oldpath = files.filetoupload.path;
-      newpath = './' + files.filetoupload.name;
+      newpath = './tmp/' + files.filetoupload.name;
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
         res.write('File uploaded and moved!');
