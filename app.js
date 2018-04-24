24b9d1dff6c138c93dad1bfb5fa4971262b24e45
rahuljain5 <rjain.rahul5@gmail.com>
2018-04-24 22:37:14 +0530

    USN Generation before scrape

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var axi = require("axios");
 var qs = require('qs');
 var S = require('string');
 var padder = require('zpad');
+var vtu = require('./utils/helper.js')
 var fetchObj = require('./utils/Result_Fetch.js');
 var conf = require('./config/config.js')
 try {
   // Call the Async process that will fetch all class results
-  fetchObj.scrape(conf.test.usn);  
+  var usn = vtu.UsnGenerator(conf.test.usn);
+  fetchObj.scrape(usn);  
 }
 catch (error) { throw(error);
 exit();
