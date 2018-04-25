cd843787104833abf772edd354db00a0727a49ee
rahuljain5 <rjain.rahul5@gmail.com>
2018-04-25 17:09:03 +0530

    Removed API Key

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   development: {
     connection_url: process.env.DATABASE_URL,
-    api_key:"99bff15d-d4fe-44dd-a0a3-25f177535450"
+    api_key:""
   },
   test: {
      connection_url: process.env.DATABASE_URL,
-     api_key:"99bff15d-d4fe-44dd-a0a3-25f177535450",
+     api_key:"",
      usn : "1ox15cs"
   },
   production: {
