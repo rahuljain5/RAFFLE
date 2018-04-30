9ac2c4f64e3cb48df0d8b3a70db03633eec078d9
Shubham <shubham.399@gmail.com>
2018-04-30 14:21:53 +0530

    Update Result.js

--- routes/Result.js
+++ routes/Result.js
@@ -7,15 +7,14 @@ var router = express.Router();
 router.get('/:id', function (req, res) {
     var result = ResultFetch.extract(req.params.id);
         console.log(result);
-        res.send(result);
-//         .then(function (Result_Json) {
-//             res.setHeader('Content-Type', 'application/json');
-//             console.log('Result Fetched and Converted');
-//             res.write(JSON.stringify(Result_Json));
-//             res.send();
-//         })
-//         .catch(err => {
-//             console.error(err);
-//         });
-});
+        .then(function (Result_Json) {
+            res.setHeader('Content-Type', 'application/json');
+            console.log('Result Fetched and Converted');
+            res.write(JSON.stringify(Result_Json));
+            res.send();
+        })
+        .catch(err => {
+            console.error(err);
+        });
+ });
 module.exports = router;
