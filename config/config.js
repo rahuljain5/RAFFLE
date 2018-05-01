1c433739eac45cd81c6da520ce1e47eeb70617a7
Shubham <shubham.399@gmail.com>
2018-05-02 00:15:27 +0530

    added redis url

--- config/config.js
+++ config/config.js
@@ -1,16 +1,19 @@
 module.exports = {
   development: {
     connection_url: process.env.DATABASE_URL,
-    api_key: ""
+    api_key: "",
+    redis_url : process.env.REDIS_URL
   },
   test: {
     connection_url: "mongodb://localhost:27017/",
     api_key: "",
-    usn: "1ox15cs"
+    usn: "1ox15cs",
+    redis_url : process.env.REDIS_URL
   },
   production: {
     connection_url: process.env.DATABASE_URL,
-    api_key: process.env.API_KEY
+    api_key: process.env.API_KEY,
+    redis_url : process.env.REDIS_URL
   },
   result_url: 'http://results.vtu.ac.in/vitaviresultcbcs/resultpage.php'
 };
