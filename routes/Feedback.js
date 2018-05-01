a72e0e46395c8b71934a1434414a35036e7064a1
rahuljain5 <rjain.rahul5@gmail.com>
2018-05-01 22:50:18 +0530

    Added Routes for Feedback

---
+++ routes/Feedback.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var DB = require('../utils/Database_Operations.js');
+
+router.all('/', function(req, res) {
+
+})
+
+router.get('/ClassRooms', function(req, res) {
+    var classrooms = DB.Find('Faculty_Feedback', 'ClassRooms');
+    res.send(classrooms);
+});
+
+router.post('/NewClassRoom', function(req, res) {
+    DB.InsertOne('Faculty_Feedback', 'ClassRooms', Class_Json);
+})
