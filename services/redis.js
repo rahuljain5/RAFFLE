ca8402681cbc6797b85ff29579feb2177a5af63a
Shubham <shubham.399@gmail.com>
2018-05-01 23:44:25 +0530

    Update redis.js

--- services/redis.js
+++ services/redis.js
@@ -5,9 +5,13 @@ const client = redis.createClient(redisURL);
 const set = function(key, value) {
   return client.set(key, value, redis.print);
 }
+const setex = function(key,value,ttl){
+return client.setexAsync(key, ttl, value);
+}
 const get = function(key, cb) {
   client.get(key, cb);
 }
+
 
 exports.redis = client;
 exports.set = set;
