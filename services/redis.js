6af06827e722164c5872b8b96e25b204c7abb643
Shubham <shubham.399@gmail.com>
2018-05-01 23:42:19 +0530

    Code added

--- services/redis.js
+++ services/redis.js
@@ -1,1 +1,14 @@
 
+var redis = require("redis");
+const client = redis.createClient(redisURL);
+
+const set = function(key, value) {
+  return client.set(key, value, redis.print);
+}
+const get = function(key, cb) {
+  client.get(key, cb);
+}
+
+exports.redis = client;
+exports.set = set;
+exports.get = get;
