1643fde4f33c319cca5c7e937099b4ade8b943d4
rahuljain5 <rjain.rahul5@gmail.com>
2018-05-02 22:53:36 +0530

    Handling JSON Request for New Classroom Creation

--- routes/Feedback.js
+++ routes/Feedback.js
@@ -4,19 +4,25 @@ var DB = require('../utils/Database_Operations.js');
 
 router.all('/', function(req, res) {
 
-})
+});
 
 router.get('/ClassRooms', function(req, res) {
     var classrooms = DB.Find('Faculty_Feedback', 'ClassRooms');
-    res.send(classrooms);
+    res.send(JSON.stringify(classrooms));
 });
 
 router.post('/NewClassRoom', function(req, res) {
-    console.log(req.body);
-    res.send("sent whatt i should have")
+    if(req.headers["content-type"]=='application/json') {
+        console.log(`New ClassRoom : ${req.body.classroom} Created at ${new Date().toLocaleString()}`);
+        res.send(JSON.stringify({status: "success"}));
+        // DB.InsertOne('Faculty_Feedback', 'ClassRooms', Class_Json);
+    }
+    else 
+    {
+        res.send(JSON.stringify({status: "Failed", message: "Improper Content Type; JSON Expected."}));
+    }
+    console.log(req.headers["content-type"]);
     
+});
 
-    
-    // DB.InsertOne('Faculty_Feedback', 'ClassRooms', Class_Json);
-})
 module.exports = router;
