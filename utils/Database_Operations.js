3bfc5d392e3c7173f5a27e3314ea28c0b1feb4f3
rahuljain5 <rjain.rahul5@gmail.com>
2018-05-03 00:00:18 +0530

    Promisified all DB Functions

--- utils/Database_Operations.js
+++ utils/Database_Operations.js
@@ -4,66 +4,76 @@ var url = Config.test.connection_url;
 
 const CreateDBCollection = (DBName, CollectionName) => {
     url = url + DBName;
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        console.log(`Database  ${DBName} created: at ${new Date().toLocaleString()}`);
-        var dbo = db.db(DBName);
-        dbo.createCollection(CollectionName, function (err, res) {
-            if (err) throw err;
-            console.log(`Database  ${CollectionName} created: at ${new Date().toLocaleString()}`);
-        });
-        db.close();
-    });
-}
-
-const InsertOne = (DBName, CollectionName, DataObject) => {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(DBName);
-        dbo.collection(CollectionName).insertOne(DataObject, function (err, res) {
-            if (err) throw err;
-            console.log(`1 document inserted into DB: ${DBName}, Collection: ${CollectionName} at ${new Date().toLocaleString()}`);
-            
-        });
-        db.close();
-    });
-}
-
-const Find = (DBName, CollectionName) => {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(DBName);
-        dbo.collection(CollectionName).find({}).toArray(function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            return result;
-        });
-        db.close();
-    });
-}
-
-const FindOne = (DBName, CollectionName) => {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(DBName);
-        dbo.collection(CollectionName).findOne({}, function (err, result) {
-            if (err) throw err;
-            console.log(result.name);
-            return result;
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, function (err, db) {
+            if (err) reject(err);
+            console.log(`Database  ${DBName} created: at ${new Date().toLocaleString()}`);
+            var dbo = db.db(DBName);
+            dbo.createCollection(CollectionName, function (err, res) {
+                if (err) reject(err);
+                console.log(`Database  ${CollectionName} created: at ${new Date().toLocaleString()}`);
+            });
             db.close();
         });
     });
 }
 
+const InsertOne = (DBName, CollectionName, DataObject) => {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, function (err, db) {
+            if (err) reject(err);
+            var dbo = db.db(DBName);
+            dbo.collection(CollectionName).insertOne(DataObject, function (err, res) {
+                if (err) reject(err);
+                else {
+                    console.log(`1 document inserted into DB: ${DBName}, Collection: ${CollectionName} at ${new Date().toLocaleString()}`);
+                    resolve(result);
+                }
+            });
+            db.close();
+        });
+    });
+}
+
+const Find = (DBName, CollectionName) => {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, function (err, db) {
+            if (err) reject(err);
+            var dbo = db.db(DBName);
+            dbo.collection(CollectionName).find({}).toArray(function (err, result) {
+                if (err) reject(err);
+                console.log(result);
+                resolve(result);
+            });
+            db.close();
+        });
+    })
+}
+
+const FindOne = (DBName, CollectionName) => {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, function (err, db) {
+            if (err) reject(err);
+            var dbo = db.db(DBName);
+            dbo.collection(CollectionName).findOne({}, function (err, result) {
+                if (err) reject(err);
+                console.log(result.name);
+                resolve(result);
+                db.close();
+            });
+        });
+    })
+}
+
 const Query = (DBName, CollectionName, Query) => {
-    return new Promise((resolve, reject)=> {
+    return new Promise((resolve, reject) => {
         MongoClient.connect(url, function (err, db) {
             if (err) reject(err);
             var dbo = db.db(DBName);
             dbo.collection(CollectionName).find(Query).toArray(function (err, result) {
                 if (err) reject(err);
                 console.log(result);
-           resolve(result);
+                resolve(result);
                 db.close();
             });
         });
@@ -71,70 +81,80 @@ const Query = (DBName, CollectionName, Query) => {
 }
 
 const DeleteOne = (DBName, CollectionName, DeleteQuery) => {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(DBName);
-        dbo.collection(CollectionName).deleteOne(myquery, function (err, obj) {
-            if (err) throw err;
-            console.log(`Mongo: ${obj.result.n} document(s) deleted from ${DBName + "->" + CollectionName } at ${new Date().toLocaleString()}`);
-            db.close();
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, function (err, db) {
+            if (err) reject(err);
+            var dbo = db.db(DBName);
+            dbo.collection(CollectionName).deleteOne(myquery, function (err, obj) {
+                if (err) reject(err);
+                console.log(`Mongo: ${obj.result.n} document(s) deleted from ${DBName + "->" + CollectionName } at ${new Date().toLocaleString()}`);
+                db.close();
+            });
         });
-    });
+    })
 }
 
 const DeleteMany = (DBName, CollectionName, DeleteQuery) => {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(DBName);
-        dbo.collection(CollectionName).deleteMany(DeleteQuery, function (err, obj) {
-            if (err) throw err;
-            console.log(`Mongo: ${obj.result.n} document(s) deleted from ${DBName + "->" + CollectionName } at ${new Date().toLocaleString()}`);
-            db.close();
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, function (err, db) {
+            if (err) reject(err);
+            var dbo = db.db(DBName);
+            dbo.collection(CollectionName).deleteMany(DeleteQuery, function (err, obj) {
+                if (err) reject(err);
+                console.log(`Mongo: ${obj.result.n} document(s) deleted from ${DBName + "->" + CollectionName } at ${new Date().toLocaleString()}`);
+                db.close();
+            });
         });
-    });
+    })
 }
 
 const Sort = (DBName, CollectionName, SortQuery) => {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(DBName);
-        dbo.collection(CollectionName).find().sort(SortQuery).toArray(function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            return result;
-            db.close();
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, function (err, db) {
+            if (err) reject(err);
+            var dbo = db.db(DBName);
+            dbo.collection(CollectionName).find().sort(SortQuery).toArray(function (err, result) {
+                if (err) reject(err);
+                console.log(result);
+                resolve(result);
+                db.close();
+            });
         });
-    });
+    })
 }
 
 const UpdateOne = (DBName, CollectionName, Query, UpdateValues) => {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(DBName);
-        var newvalues = {
-            $set: UpdateValues
-        };
-        dbo.collection(CollectionName).updateOne(Query, newvalues, function (err, res) {
-            if (err) throw err;
-            console.log(res.result.nModified + ` document(s) updated at ${new Date().toLocaleString()}`);
-            return result;
-            db.close();
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, function (err, db) {
+            if (err) reject(err);
+            var dbo = db.db(DBName);
+            var newvalues = {
+                $set: UpdateValues
+            };
+            dbo.collection(CollectionName).updateOne(Query, newvalues, function (err, res) {
+                if (err) reject(err);
+                console.log(res.result.nModified + ` document(s) updated at ${new Date().toLocaleString()}`);
+                resolve(result);
+                db.close();
+            });
         });
-    });
+    })
 }
 const UpdateMany = (DBName, CollectionName, Query, UpdateValues) => {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(DBName);
-        var newvalues = {
-            $set: UpdateValues
-        };
-        dbo.collection(CollectionName).updateMany(Query, newvalues, function (err, res) {
-            if (err) throw err;
-            console.log(res.result.nModified + ` document(s) updated at ${new Date().toLocaleString()}`);
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, function (err, db) {
+            if (err) reject(err);
+            var dbo = db.db(DBName);
+            var newvalues = {
+                $set: UpdateValues
+            };
+            dbo.collection(CollectionName).updateMany(Query, newvalues, function (err, res) {
+                if (err) reject(err);
+                console.log(res.result.nModified + ` document(s) updated at ${new Date().toLocaleString()}`);
+            });
+            db.close();
         });
-        db.close();
-    });
+    })
 }
 
 
