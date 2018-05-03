1180161ea169103a705e678f19fd05a2f4690f83
rahuljain5 <rjain.rahul5@gmail.com>
2018-05-03 19:26:00 +0530

    Promisify Function Calls

--- routes/Feedback.js
+++ routes/Feedback.js
@@ -14,19 +14,31 @@ router.get('/:id', function(req, res) {
         })
     .catch(err=>{
         console.error("An Error Occoured getting the Class" + req.params.id);
+        console.error(err);
     })
 });
 
 router.get('/ClassRooms', function(req, res) {
-    var classrooms = DB.Find('Faculty_Feedback', 'ClassRooms');
-    res.send(JSON.stringify(classrooms));
+    DB.Find('Faculty_Feedback', 'ClassRooms')
+    .then(function(classrooms){
+        res.send(JSON.stringify(classrooms));
+    })
+    .catch(err=>{
+        console.error("Error Occoured getting the Class Room :" + err);
+        
+    })
 });
 
 router.post('/NewClassRoom', function(req, res) {
     if(req.headers["content-type"]=='application/json') {
-        console.log(`New ClassRoom : ${req.body.classroom} Created at ${new Date().toLocaleString()}`);
-        res.send(JSON.stringify({status: "success"}));
-        DB.InsertOne('Faculty_Feedback', 'ClassRooms', req.body);
+        DB.InsertOne('Faculty_Feedback', 'ClassRooms', req.body)
+        .then(function(result){
+            console.log(`New ClassRoom : ${req.body.classroom} Created at ${new Date().toLocaleString()}`);
+            res.send(JSON.stringify({status: "success"}));
+        })
+        .catch(err=>{
+            console.error("Error Occured Creating New ClassRoom");
+        })
     }
     else 
     {
