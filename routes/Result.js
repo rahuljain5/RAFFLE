ef93259a521f47689220208016362fe6262ae1c2
Rahul Jain <rjain.rahul5@gmail.com>
2018-05-03 13:47:05 +0530

    Promise resolve into Backend UAT (#11)
    * Mongo Package added and File Permission changed
    
    * Server start message edit
    
    * Intendation Fix and Minor Fixes
    
    * Converted Result Fetch to a promise and calls to resolve/reject
    
    * Removed Files that belong to another Branch
    
    * Added Respone Write
    
    * Update Result_Fetch.js
    
    * Code Refactored
    
    * Promise Resolve update
    
    * Update RangeResult.js
    
    * adding log
    
    * commented Console.logs
    
    * added [0] to get the first element from the array
    
    * extact function added 
    
    slipted scrape function
    
    * added reject case for results not found
    
    * Update Result.js
    
    * Update Result.js
    
    * Update Result.js
    
    * Update Result_Fetch.js
    
    * Update Result.js
    
    * Update Result.js
    
    * Update Result_Fetch.js
    
    * Update Result.js
    
    * Update Result.js
    
    * Update Result_Fetch.js
    
    * removed console.log
    
    * Update Result_Fetch.js
    
    * Update RangeResult.js
    
    * Update RangeResult.js
    
    * Update RangeResult.js
    
    * Update RangeResult.js
    
    * Update RangeResult.js
    
    * Update package.json
    
    * Update RangeResult.js
    
    * Update RangeResult.js
    
    * Update RangeResult.js
    
    * Update helper.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update RangeResult.js
    
    * Update package.json
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update app.js
    
    * Update package.json
    
    * Update app.js
    
    * CSV Result call promisified
    
    * Class Result call promisified
    
    * CSV Result Response Fix
    
    * Enable Successful fetch log
    
    * minnor fix
    
    * Creating Redis Services
    
    * Code added
    
    * Update redis.js
    
    * Update redis.js
    
    * Update redis.js
    
    * added redis url
    
    * added redis url
    
    * Update Result_Fetch.js
    
    * Update package.json
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update redis.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update redis.js
    
    * Update Result_Fetch.js
    
    * Update redis.js
    
    * Update redis.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update config.js
    
    * Update Result_Fetch.js
    
    * Update config.js
    
    * PORT CHANGE
    
    * USN Generator Bug Fix

--- routes/Result.js
+++ routes/Result.js
@@ -4,8 +4,15 @@ var ResultFetch = require('../utils/Result_Fetch.js');
 var config = require('../config/config.js')
 var router = express.Router();
 
-router.get('/:id', function(req, res) {
-    ResultFetch.scrape([req.params.id]);
-});
-
+router.get('/:id', function (req, res) {
+    ResultFetch.scrape([req.params.id])[0].then(function (Result_Json) {
+            res.setHeader('Content-Type', 'application/json');
+            console.log('Result Fetched and Converted');
+            res.write(JSON.stringify(Result_Json));
+            res.send();
+        })
+        .catch(err => {
+            console.error(err);
+        });
+ });
 module.exports = router;
--- utils/Result_Fetch.js
+++ utils/Result_Fetch.js
@@ -2,57 +2,83 @@ var axios = require("axios");
 var qs = require('qs');
 var S = require('string');
 var jsdom = require('jsdom');
-var asyncProcess = require('async-process');
 var padder = require('zpad');
 var Helper = require('./helper.js');
 var config = require('../config/config.js');
-const {JSDOM} = jsdom;
-//Function that scrapes results from VTU 
-const scrape = (usn) => {
-    //Function that will be called Asynchronously
-    var scraper = function (rusn, cb) {
-        //Send Request to VTU with string USN as POST 
-        axios.post(config.result_url, qs.stringify({ lns: rusn }))
-            .then(function (response) {
+const redis = require("../services/redis")
+// var DB = require('./Database_Operations.js');
+const {
+  JSDOM
+} = jsdom;
+//Function that scrapes results from VTU
 
-                var str = S(response.data);
-                //Check if Result Exists or Not
-                if (str.contains("alert(\"University Seat Number is not available or Invalid..!\");") != false) 
-                {
-                    console.log(rusn + "Failed/Doesn't Exist");
+const extract = (usn) => {
+  return new Promise((resolve, reject) => {
+    redis.get(usn, (err, cachedData) => {
+      console.log("USN: " + usn + " Cached Data: " + cachedData);
+      if (cachedData != undefined && cachedData != null) {
+        console.log("Resolve data from redis");
+        resolve(JSON.parse(cachedData));
+      } else {
+        console.log("Check Result for " + usn + " at URL:" + config.result_url);
+        axios.post(config.result_url, qs.stringify({
+            lns: usn
+          }))
+          .then(function(response) {
+            var str = S(response.data);
+            if (response.status == 200) {
+              if (str.contains("alert(\"University Seat Number is not available or Invalid..!\");") != false) {
+                //                             console.log(usn + " Failed/Doesn't Exist");
+                let failresponse = {
+                  error: true,
+                  errorMessage: "USN not found " + usn,
+                  userMessage: usn + " Failed/Doesn't Exist"
                 }
-                else
-                {
-                    var str, sems=[];
-                    var Json = {};    
-                // fs.readFile('sample result.html', 'utf8',function(err, str) {
-                    //Convert Response HTML into DOM Object
-                    var parser = new JSDOM(str);
-                    //Access Result Tables using their Class
-                    var tables = parser.window.document.getElementsByClassName("divTable");
-                    //Get the Semesters for which Results are being displayed
-                    sems = Helper.getSemesters(tables, str);
-                    //Get Name and USN of Student
-                    Json = Helper.getNameUsn(parser, Json);
-                    //Get the Final JSON
-                    Json.Results = Helper.ResultJsonParser(tables, sems);
-                    console.log(Json);
-                    // console.log(JSON.stringify(Json));
-                }
-            })
-            .catch(err => {
-                console.error("Failed with Status Code" + err.response.status);
+                //redis.setex(usn, JSON.stringify(failresponse), config.result_ttl);
+                resolve(failresponse);
+              } else {
+                var str, sems = [];
+                var responeData = {};
+                var parser = new JSDOM(str);
+                var tables = parser.window.document.getElementsByClassName("divTable");
+                sems = Helper.getSemesters(tables, str);
+                responeData = Helper.getNameUsn(parser, responeData);
+                responeData.Results = Helper.ResultJsonParser(tables, sems);
+                //                           	console.log("Inside Axios Respone => "+ responeData)
+                responeData["error"] = false;
+                console.log("Set data in Redis");
+                redis.setex(usn, JSON.stringify(responeData), config.result_ttl); //TTL 10min
+                //                             redis.set(usn,JSON.stringify(responeData));
+                console.log("Resolve Response");
+                resolve(responeData);
+                console.log(`${usn}: Result Fetch Completed`);
+              }
+            } else {
+              var error = `Request Returned ${response.status}: ${response.statusText}`;
+              resolve({
+                error: true,
+                errorMessage: error,
+                userMessage: "Unable to Process the Request"
               });
-    }
+            }
+          })
+          .catch(err => {
+            console.error("Connection could not be established.");
+            resolve({
+              error: true,
+              errorMessage: err,
+              userMessage: "Unable to Process the Request"
+            });
+          });
+      }
+    })
+  });
 
-    //Asynchronously send Requests
-    new asyncProcess(usn, scraper, function () {
-        //At this point all the USN requests are sent
-        console.log("All usn have been processed");
-        this.tasksFails.forEach(function (id) {
-            console.warn("Requesting usn : " + id + " failed.");
-        })
-    });
+}
+
+const scrape = (USNs) => {
+  return USNs.map(extract);
 }
 
 exports.scrape = scrape;
+exports.extract = extract;
