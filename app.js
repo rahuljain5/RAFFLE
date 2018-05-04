7d137d0003778166bc12fc440c6d704e5bf02cbd
rahuljain5 <rjain.rahul5@gmail.com>
2018-05-05 00:30:10 +0530

    Fixed Update Variable Bug

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ var ClassResult = require('./routes/ClassResult.js');
 var file_handler = require('./utils/FileHandler.js');
 var RangeResult = require('./routes/RangeResult.js');
 var Result = require('./routes/Result.js');
-
-const initmiddleware = (app) =>{
 var Feedback = require('./routes/Feedback.js');
-var upload = multer({ dest: 'tmp/' });
+var upload;
+const initmiddleware = (app) =>{
+upload = multer({ dest: 'tmp/' });
 var app = express();
   
 // view engine setup
