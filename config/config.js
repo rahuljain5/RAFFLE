0a3ac8bd2eba23a82ecf877fe2e279047ace1902
Rahul Jain <rjain.rahul5@gmail.com>
2018-05-04 23:49:28 +0530

    Promise resolve (#14)
    * Intendation Fix and Minor Fixes
    
    * Converted Result Fetch to a promise and calls to resolve/reject
    
    * Removed Files that belong to another Branch
    
    * Added Respone Write
    
    * Update Result_Fetch.js
    
    * Code Refactored
    
    * Promise Resolve update
    
    * Update RangeResult.js
    
    * adding log
    
    * commented Console.logs
    
    * added [0] to get the first element from the array
    
    * extact function added 
    
    slipted scrape function
    
    * added reject case for results not found
    
    * Update Result.js
    
    * Update Result.js
    
    * Update Result.js
    
    * Update Result_Fetch.js
    
    * Update Result.js
    
    * Update Result.js
    
    * Update Result_Fetch.js
    
    * Update Result.js
    
    * Update Result.js
    
    * Update Result_Fetch.js
    
    * removed console.log
    
    * Update Result_Fetch.js
    
    * Update RangeResult.js
    
    * Update RangeResult.js
    
    * Update RangeResult.js
    
    * Update RangeResult.js
    
    * Update RangeResult.js
    
    * Update package.json
    
    * Update RangeResult.js
    
    * Update RangeResult.js
    
    * Update RangeResult.js
    
    * Update helper.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update RangeResult.js
    
    * Update package.json
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update app.js
    
    * Update package.json
    
    * Update app.js
    
    * CSV Result call promisified
    
    * Class Result call promisified
    
    * CSV Result Response Fix
    
    * Enable Successful fetch log
    
    * minnor fix
    
    * Creating Redis Services
    
    * Code added
    
    * Update redis.js
    
    * Update redis.js
    
    * Update redis.js
    
    * added redis url
    
    * added redis url
    
    * Update Result_Fetch.js
    
    * Update package.json
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update redis.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update redis.js
    
    * Update Result_Fetch.js
    
    * Update redis.js
    
    * Update redis.js
    
    * Update Result_Fetch.js
    
    * Update Result_Fetch.js
    
    * Update config.js
    
    * Update Result_Fetch.js
    
    * Update config.js
    
    * PORT CHANGE
    
    * USN Generator Bug Fix

--- config/config.js
+++ config/config.js
@@ -1,16 +1,20 @@
 module.exports = {
   development: {
     connection_url: process.env.DATABASE_URL,
-    api_key:""
+    api_key: "",
+    redis_url : process.env.REDIS_URL || "redis://localhost:6379"
   },
   test: {
-     connection_url: process.env.DATABASE_URL,
-     api_key:"",
-     usn : "1ox15cs"
+    connection_url: "mongodb://localhost:27017/",
+    api_key: "",
+    usn: "1ox15cs",
+    redis_url : process.env.REDIS_URL
   },
   production: {
-   connection_url: process.env.DATABASE_URL,
-   api_key:process.env.API_KEY
+    connection_url: process.env.DATABASE_URL,
+    api_key: process.env.API_KEY,
+    redis_url : process.env.REDIS_URL
   },
-  result_url:'http://results.vtu.ac.in/vitaviresultcbcs/resultpage.php'
+  result_url: 'http://results.vtu.ac.in/vitaviresultcbcs/resultpage.php',
+  result_ttl : 60*10//10mins
 };
--- routes/ClassResult.js
+++ routes/ClassResult.js
@@ -4,18 +4,23 @@ var ResultFetch = require('../utils/Result_Fetch.js');
 var config = require('../config/config.js')
 var router = express.Router();
 
-router.all('/', function(req, res) {
-try {
-  // Generate USNs for class results
-  var usn = vtu.UsnGenerator(config.test.usn);
-  //Scrape and get Results
-  ResultFetch.scrape(usn);  
-  console.log('Class Result Fetched and Converted');
-  res.send('Class Result Fetched and Converted');
-}
-catch (error) { throw(error);
-exit();
-}
+router.all('/', function (req, res) {
+  try {
+    // Generate USNs for class results
+    var USNs = vtu.UsnGenerator(config.test.usn);
+    //Scrape and get Results
+    Promise.all(ResultFetch.scrape(USNs)).then(function(values) {
+      // console.log(values);
+      console.log("Class Results Retrieved");
+      res.send(values);
+    })
+      .catch(err => {
+        console.error(err);
+      });
+  } catch (error) {
+    throw (error);
+    exit();
+  }
 });
 
 module.exports = router;
