d1748ba4aa62b3d39eb8d1e9e974f44ca4c5c719
Rahul Jain <rjain.rahul5@gmail.com>
2018-05-05 10:59:59 +0530

    Commented Feedback

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var RangeResult = require('./routes/RangeResult.js');
 var Result = require('./routes/Result.js');
 
 const initmiddleware = (app) =>{
-var Feedback = require('./routes/Feedback.js');
+// var Feedback = require('./routes/Feedback.js');
 var upload;
 const initmiddleware = (app) =>{
 upload = multer({ dest: 'tmp/' });
@@ -46,7 +46,7 @@ const initroutes = (app) => {
 
   app.use("/Result", Result);
 
-  app.use("/Feedback", Feedback);
+//   app.use("/Feedback", Feedback);
 
   app.post('/CSVResult', upload.single('filetoupload'), function (req, res) {
     console.log("File Saved At:" + req.file.path);
