b95151b7e798e366548f41691ba582b85cd8cb65
rahuljain5 <rjain.rahul5@gmail.com>
2018-05-08 00:17:26 +0530

    Push Results into DB

--- services/Result_Fetch.js
+++ services/Result_Fetch.js
@@ -5,6 +5,7 @@ var jsdom = require('jsdom');
 var padder = require('zpad');
 var Helper = require('../utils/helper.js');
 var config = require('../config/config.js');
+var DB = require('../utils/Database_Operations.js');
 const redis = require("../services/redis");
 // var DB = require('./Database_Operations.js');
 const {
@@ -43,11 +44,23 @@ const extract = (usn) => {
                 var tables = parser.window.document.getElementsByClassName("divTable");
                 sems = Helper.getSemesters(tables, str);
                 responseData = Helper.getNameUsn(parser, responseData);
-                responseData.timestamp = `${date.getMonth()}-${date.getFullYear()}`;
+                responseData.Yearstamp = `${date.getFullYear()}`;
                 responseData.Results = Helper.ResultJsonParser(tables, sems);
                 //                           	console.log("Inside Axios Respone => "+ responseData)
                 responseData["error"] = false;
                 console.log("Set data in Redis");
+                DB.InsertOne('Result_analyzer', 'Results', responseData)
+                .then(function (result) {
+                    console.log(`${usn} Result pushed into DB at: ${new Date().toLocaleString()}`);
+                })
+                .catch(err => {
+                    let failresponse = {
+                        error: true,
+                        errorMessage: err,
+                        userMessage: "An Error Occoured pushing into DB"
+                    }
+                    console.error(failresponse);
+                  })
                 redis.setex(usn, JSON.stringify(responseData), config.result_ttl); //TTL 14Days
                 //                             redis.set(usn,JSON.stringify(responseData));
                 console.log("Resolve Response");
