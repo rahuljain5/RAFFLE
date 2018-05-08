b18ab1aefb61f757f9941dbb21c17b3fed0ca2f2
rahuljain5 <rjain.rahul5@gmail.com>
2018-05-08 17:10:42 +0530

    Added Analysis Feature

--- routes/Result.js
+++ routes/Result.js
@@ -2,44 +2,50 @@ var express = require('express');
 var vtu = require('../utils/helper.js')
 var ResultFetch = require('../services/Result_Fetch.js');
 var config = require('../config/config.js');
+const redis = require("../services/redis.js");
 var ResultAnalyzer = require('../utils/ResultAnalyze');
 var router = express.Router();
 
-router.get('/Analyze', function(req, res){
-    
-    Promise.all(ResultAnalyzer.Results())
-    .then(function (values) {
-        ResultAnalyzer.getTotalResultsCount( function (count) {
-            console.log(count);
-            values.sort("_id.Subcode");
-            // var fbjson = FeedbackAnalyze.toFeedbackJson(values, count);
-            // console.log("Setting Value in Redis");
-            // redis.setex("Analyze" + req.query.classroom + req.query.batch, JSON.stringify(values), config.result_ttl);
-            res.send(JSON.stringify(values));
-        })
+router.post('/Analyze', function (req, res) {
+    redis.get("Analyzed" + req.query.semester + "." + req.query.year, (err, cachedData) => {
+        if (!err) {
+            if (cachedData == null || cachedData == undefined) {
+                Promise.all(ResultAnalyzer.Results(req.query.year, req.query.semester))
+                    .then(function (values) {
+                        ResultAnalyzer.getTotalResultsCount(function (count) {
+                            var AnalysisReport = ResultAnalyzer.toAnalyzedJson(values, count[0].total);
+                            res.send(JSON.stringify(AnalysisReport));
+                            console.log("Setting Value in Redis");
+                            redis.setex("Analyzed" + req.query.semester + "." + req.query.year, JSON.stringify(AnalysisReport), config.result_ttl);
+                        })
+                    })
+                    .catch(err => {
+                        res.send(JSON.stringify({
+                            error: true,
+                            message: err,
+                            userMessage: "Improper Content Type; JSON Expected."
+                        }));
+                        console.error(err);
+                    })
+            } else {
+                console.log("Getting Value from Redis");
+                res.send(cachedData);
+            }
+        } else
+            console.error(err);
     })
-    .catch(err => {
-        res.send(JSON.stringify({
-            error: true,
-            message: err,
-            userMessage: "Improper Content Type; JSON Expected."
-        }));
-        console.error(err);
-    })
-
- })
+})
 
 router.get('/:id', function (req, res) {
     ResultFetch.scrape([req.params.id])[0].then(function (Result_Json) {
             res.setHeader('Content-Type', 'application/json');
             console.log('Result Fetched and Converted');
-            res.write(JSON.stringify(Result_Json));
-            res.send();
+            res.send(JSON.stringify(Result_Json));
         })
         .catch(err => {
             console.error(err);
         });
- });
+});
 
- 
+
 module.exports = router;
