0a3d852b783cfe45f8c7ac71877a1bdbb3bfe6a0
Rahul Jain <rjain.rahul5@gmail.com>
2018-05-10 02:15:49 +0530

    Update RangeResult.js

--- routes/RangeResult.js
+++ routes/RangeResult.js
@@ -7,8 +7,8 @@ var router = express.Router();
 router.get('/', function (req, res) {
   try {    
     console.log(JSON.stringify(req.params)+"\n"+JSON.stringify(req.query))
-    console.log("Received Request For USN:"+req.query.baseusn+" Range :"+req.params.startusn+"-"+req.params.endusn)
-    Helper.RangeUsnGenerator(req.params.baseusn, req.params.startusn, req.params.endusn, function (USNs) {
+    console.log("Received Request For USN:"+req.query.baseusn+" Range :"+req.query.startusn+"-"+req.query.endusn)
+    Helper.RangeUsnGenerator(req.query.baseusn, req.query.startusn, req.query.endusn, function (USNs) {
 //       res.setHeader('Content-Type', 'application/json');
 //       console.log(USNs);
       //Scrape and get Results
