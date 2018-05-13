bbea8078effa8df8604339de78aae27d7b50c944
Shubham <shubham.399@gmail.com>
2018-05-13 10:44:31 +0530

    allow sessionkey over cros

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "access-control-allow-origin");
+  res.header("Access-Control-Allow-Headers", "access-control-allow-origin,X-SESSION-KEY");
   next();
  });
 
