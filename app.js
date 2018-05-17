41f8025785a288533ac9e610af03702d45f7a911
Shubham <shubham.399@gmail.com>
2018-05-17 16:12:15 +0530

    REmoved  SET FOREIGN_KEY_CHECKS

--- app.js
+++ app.js
@@ -93,22 +93,33 @@ const startserver = (app) => {
 
 if(cluster.isMaster) {
     var numWorkers = require('os').cpus().length;
-  models
-    .sequelize
-    .query('SET FOREIGN_KEY_CHECKS = 0', {raw: true})
-    .then(function(results) {
-    models.sequelize.sync({
+  models.sync({{
     force: config.resetdb
-  }).then(()=>{
-    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+  }}).then(()=>{
+     console.log('Master cluster setting up ' + numWorkers + ' workers...');
 
     for(var i = 0; i < numWorkers; i++) {
         cluster.fork();
     }})
-    .catch((err) =>{
+     .catch((err) =>{
       console.error("Erorr: "+err)
     })
-  });
+//   models
+//     .sequelize
+// //     .query('SET FOREIGN_KEY_CHECKS = 0', {raw: true})
+//     .then(function(results) {
+//     models.sequelize.sync({
+//     force: config.resetdb
+//   }).then(()=>{
+//     console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+//     for(var i = 0; i < numWorkers; i++) {
+//         cluster.fork();
+//     }})
+//     .catch((err) =>{
+//       console.error("Erorr: "+err)
+//     }) 
+//   });
     cluster.on('online', function(worker) {
         console.log('Worker ' + worker.process.pid + ' is online');
     });
