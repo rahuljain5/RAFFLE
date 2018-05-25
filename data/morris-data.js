e3ef6bcad3d721a1326635c467df9b3365abbd7c
rahuljain5 <rjain.rahul5@gmail.com>
2018-05-25 07:27:12 +0530

    bar chart update

--- data/morris-data.js
+++ data/morris-data.js
@@ -81,43 +81,15 @@ function donut(elementid, Values, totalstuds){
     });
 }
     
-function Bar() {
+function Bar(elementid, stats, colorStatus) {
     Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            y: '2006',
-            a: 100,
-            b: 90
-        }, {
-            y: '2007',
-            a: 75,
-            b: 65
-        }, {
-            y: '2008',
-            a: 50,
-            b: 40
-        }, {
-            y: '2009',
-            a: 75,
-            b: 65
-        }, {
-            y: '2010',
-            a: 50,
-            b: 40
-        }, {
-            y: '2011',
-            a: 75,
-            b: 65
-        }, {
-            y: '2012',
-            a: 100,
-            b: 90
-        }],
+        element: elementid,
+        data: stats,
         xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
+        ykeys: 'a',
+        labels: 'Series A',
+        barColors: colorStatus,
         hideHover: 'auto',
         resize: true
     });
-    
 }
