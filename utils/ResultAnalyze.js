79a7e4c30a10419a823478eb7a46779cd305d6cf
poornac <37918447+poornac@users.noreply.github.com>
2018-06-14 10:16:08 +0530

    Update ResultAnalyze.js

--- utils/ResultAnalyze.js
+++ utils/ResultAnalyze.js
@@ -114,7 +114,7 @@ const Results = (year, semester) => {
 
 const toAnalyzedJson = (ResultStatsArray, Count) => {
     var SubjectJson = {};
-    console.log(ResultStatsArray)
+    console.log(JSON.stringify(ResultStatsArray))
     ResultStatsArray.forEach(SubArray => {
         SubArray.forEach(Res => {
             SubjectJson[Res._id.subCode] = {
