f020894ad8890f36ec2f040234ad0521acd1687c
rahuljain5 <rjain.rahul5@gmail.com>
2018-06-23 18:18:05 +0530

    bug fix

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ const initroutes = (app) => {
 
   app.use('/users', users);
 
-  app.use('/authenticate', function(){
+  app.use('/authenticate', function(req, res) {
     res.send({error: false,
     session: "active"})
   });
