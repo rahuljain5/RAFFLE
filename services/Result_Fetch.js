1419d06dce539fe37f9d989f3621e9a3489e4f9f
Rahul Jain <rjain.rahul5@gmail.com>
2018-08-29 08:56:05 +0530

    Update Result_Fetch.js

--- services/Result_Fetch.js
+++ services/Result_Fetch.js
@@ -22,81 +22,80 @@ const extract = (usn) => {
         resolve(JSON.parse(cachedData));
       } else {
         Helper.getToken()
-          .then(function(token)
-                { 
-          tokenId = token
-          })
-          .catch (err => {
-            console.error("Token Could not be Found."+err);
-        });
-        console.log("Check Result for " + usn + " at URL:" + config.result_url);
-        axios.post(config.result_url, qs.stringify({
-            lns: usn,
-            token: tokenId
-          }))
-          .then(function(response) {
-            var str = S(response.data);
-            if (response.status == 200) {
-              if (str.contains("alert(\"University Seat Number is not available or Invalid..!\");") != false) {
-                //                             console.log(usn + " Failed/Doesn't Exist");
-                let failresponse = {
-                  error: true,
-                  errorMessage: "USN not found " + usn,
-                  userMessage: usn + " Failed/Doesn't Exist"
-                }
-                //redis.setex(usn, JSON.stringify(failresponse), config.result_ttl);
-                resolve(failresponse);
-              } else {
-                var str, sems = [];
-                var responseData = {};
-                var date = new Date();
-                var parser = new JSDOM(str);
-                var tables = parser.window.document.getElementsByClassName("divTable");
-                sems = Helper.getSemesters(tables, str);
-                responseData = Helper.getNameUsn(parser, responseData);
-                responseData.Yearstamp = `${date.getFullYear()}`;
-                responseData.Results = Helper.ResultJsonParser(tables, sems);
-                //                           	console.log("Inside Axios Respone => "+ responseData)
-                responseData["error"] = false;
-                console.log("Set data in Redis");
-                DB.InsertOne('Result_analyzer', 'Results', responseData)
-                .then(function (result) {
-                    console.log(`${usn} Result pushed into DB at: ${new Date().toLocaleString()}`);
-                })
-                .catch(err => {
+          .then(function (token) {
+            tokenId = token
+            console.log("Check Result for " + usn + " at URL:" + config.result_url);
+            axios.post(config.result_url, qs.stringify({
+              lns: usn,
+              token: tokenId
+            }))
+              .then(function (response) {
+                var str = S(response.data);
+                if (response.status == 200) {
+                  if (str.contains("alert(\"University Seat Number is not available or Invalid..!\");") != false) {
+                    //                             console.log(usn + " Failed/Doesn't Exist");
                     let failresponse = {
-                        error: true,
-                        errorMessage: err,
-                        userMessage: "An Error Occoured pushing into DB"
+                      error: true,
+                      errorMessage: "USN not found " + usn,
+                      userMessage: usn + " Failed/Doesn't Exist"
                     }
-                    console.error(failresponse);
-                  })
-                redis.setex(usn, JSON.stringify(responseData), config.result_ttl); //TTL 14Days
-                //                             redis.set(usn,JSON.stringify(responseData));
-                console.log("Resolve Response");
-                resolve(responseData);
-                console.log(`${usn}: Result Fetch Completed`);
-              }
-            } else {
-              var error = `Request Returned ${response.status}: ${response.statusText}`;
-              resolve({
-                error: true,
-                errorMessage: error,
-                userMessage: "Unable to Process the Request"
+                    //redis.setex(usn, JSON.stringify(failresponse), config.result_ttl);
+                    resolve(failresponse);
+                  } else {
+                    var str, sems = [];
+                    var responseData = {};
+                    var date = new Date();
+                    var parser = new JSDOM(str);
+                    var tables = parser.window.document.getElementsByClassName("divTable");
+                    sems = Helper.getSemesters(tables, str);
+                    responseData = Helper.getNameUsn(parser, responseData);
+                    responseData.Yearstamp = `${date.getFullYear()}`;
+                    responseData.Results = Helper.ResultJsonParser(tables, sems);
+                    //                           	console.log("Inside Axios Respone => "+ responseData)
+                    responseData["error"] = false;
+                    console.log("Set data in Redis");
+                    DB.InsertOne('Result_analyzer', 'Results', responseData)
+                      .then(function (result) {
+                        console.log(`${usn} Result pushed into DB at: ${new Date().toLocaleString()}`);
+                      })
+                      .catch(err => {
+                        let failresponse = {
+                          error: true,
+                          errorMessage: err,
+                          userMessage: "An Error Occoured pushing into DB"
+                        }
+                        console.error(failresponse);
+                      })
+                    redis.setex(usn, JSON.stringify(responseData), config.result_ttl); //TTL 14Days
+                    //                             redis.set(usn,JSON.stringify(responseData));
+                    console.log("Resolve Response");
+                    resolve(responseData);
+                    console.log(`${usn}: Result Fetch Completed`);
+                  }
+                } else {
+                  var error = `Request Returned ${response.status}: ${response.statusText}`;
+                  resolve({
+                    error: true,
+                    errorMessage: error,
+                    userMessage: "Unable to Process the Request"
+                  });
+                }
+              })
+              .catch(err => {
+                console.error("Connection could not be established.");
+                resolve({
+                  error: true,
+                  errorMessage: err,
+                  userMessage: "Unable to Process the Request"
+                });
               });
-            }
-          })
-          .catch(err => {
-            console.error("Connection could not be established.");
-            resolve({
-              error: true,
-              errorMessage: err,
-              userMessage: "Unable to Process the Request"
-            });
-          });
-      }
+          }
     })
   });
+})
+          .catch (err => {
+  console.error("Token Could not be Found." + err);
+});
 }
 
 const scrape = (USNs) => {
